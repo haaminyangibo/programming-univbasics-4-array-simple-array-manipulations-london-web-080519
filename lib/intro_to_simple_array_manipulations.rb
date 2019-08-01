70ae7fa44239dfcf6020b693d3584a6d509a67be
Haami Nyangibo <haaminyangibo@gmail.com>
2019-08-01 12:19:38 +0000

    Automatically backed up by Learn

--- lib/intro_to_simple_array_manipulations.rb
+++ lib/intro_to_simple_array_manipulations.rb
@@ -2,3 +2,10 @@ def using_push
   arr = ["hf", "jk"]
 array.push("sstrong")
 end
+
+def using_unshift
+  boroughs_in_nyc = ['queens', 'something']
+  new_neighbourhood = ['lol']
+  updated_array = boroughs_in_nyc.unshist(new_neighbourhood)
+end
+  
